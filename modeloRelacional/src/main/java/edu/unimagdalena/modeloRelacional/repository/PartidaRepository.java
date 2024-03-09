63402cc0b0ce2cc6b6860620fcb610daac312b65
Jcenteno06 <96033269+KhadaReiN@users.noreply.github.com>
2024-03-09 12:07:37 -0500

    Update PartidaRepository.java

--- modeloRelacional/src/main/java/edu/unimagdalena/modeloRelacional/repository/PartidaRepository.java
+++ modeloRelacional/src/main/java/edu/unimagdalena/modeloRelacional/repository/PartidaRepository.java
@@ -3,4 +3,8 @@ package edu.unimagdalena.modeloRelacional.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import edu.unimagdalena.modeloRelacional.entities.Partida;
 
-public interface PartidaRepository extends JpaRepository<Partida,Long>{ }
+public interface PartidaRepository extends JpaRepository<Partida,Long>{
+    List<Partida> findPartidasByProvincia(String provincia);
+    List<Partida> findPartidasByCreador(String Creador);
+  
+}
